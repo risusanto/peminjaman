f3c24c35873852bdce272e0f6e1520604dbabb02
Ari Susanto <risusanto@students.ilkom.unsri.ac.id>
2017-08-16 14:54:24 +0700

    admin, login

---
+++ application/controllers/Admin.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ *
+ */
+class Admin extends MY_Controller
+{
+
+  function __construct()
+  {
+    parent::__construct();
+    $this->data['id_role'] = $this->session->userdata('id_role');
+        if (!isset($this->data['id_role']) || $this->data['id_role'] != 1)
+        {
+            $this->session->unset_userdata('username');
+            $this->session->unset_userdata('id_role');
+            $this->flashmsg('Anda harus login dulu','warning');
+            redirect('login');
+            exit;
+        }
+  }
+
+  public function index()
+  {
+    $this->data['title'] ='DASHBOARD ADMIN';
+    $this->data['content'] = 'admin/dashboard';
+    $this->template($this->data);
+  }
+}
---
+++ application/controllers/Login.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ *
+ */
+class Login extends MY_Controller
+{
+
+  function __construct()
+  {
+    parent::__construct();
+    $username = $this->session->userdata('username');
+		if (isset($username))
+		{
+			$this->data['id_role'] = $this->session->userdata('id_role');
+			switch ($this->data['id_role'])
+			{
+				case 1:
+					redirect('admin');
+					break;
+				case 2:
+					redirect('kapolres');
+					break;
+			}
+
+			exit;
+		}
+    $this->load->model('User_m');
+  }
+
+  public function index()
+  {
+    if ($this->POST('login-submit'))
+		{
+      if (!$this->User_m->required_input(['username','password'])) {
+        $this->flashmsg('Data harus lengkap','warning');
+        redirect('login');
+      }
+			$this->data = [
+				'username'	=> $this->POST('username'),
+				'password'	=> md5($this->POST('password'))
+			];
+
+			$result = $this->User_m->login($this->data);
+      if (!isset($result)) {
+        $this->flashmsg('Username atau password salah','danger');
+      }
+			redirect('login');
+			exit;
+		}
+    $this->data['title'] = 'LOGIN'.$this->title;
+    $this->load->view('login',$this->data);
+  }
+}
